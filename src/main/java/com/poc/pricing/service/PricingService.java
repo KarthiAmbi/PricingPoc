4c997c6b14f3898267b03e2d0480d9b623370ab1
karth <karthiambikapathi@gmail.com>
2019-07-29 17:01:18 +0530

    Adding new class

---
+++ src/main/java/com/poc/pricing/service/PricingService.java
@@ -0,0 +1,17 @@
+package com.poc.pricing.service;
+
+import java.util.List;
+
+import com.poc.pricing.model.PricingDto;
+
+public interface PricingService {
+
+	public PricingDto findPriceById(Long id);
+
+	public PricingDto updatePricing(PricingDto pricing, Long id);
+
+	public List<PricingDto> getAllPricing();
+
+	public PricingDto createPricing(PricingDto pricing);
+	
+}
