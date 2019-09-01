9b6cd15b561d24dd7bb93996ca11860efb00bb5d
karth <karthiambikapathi@gmail.com>
2019-09-01 22:17:09 +0530

    Jwt Auth service Integration

--- src/main/java/com/poc/pricing/filter/AuthorizationFilter.java
+++ src/main/java/com/poc/pricing/filter/AuthorizationFilter.java
@@ -1,7 +1,8 @@
-package com.poc.pricing;
+package com.poc.pricing.filter;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Date;
 
@@ -11,11 +12,20 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poc.pricing.dto.ErrorDetail;
@@ -25,7 +35,14 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Order(1)
 @Slf4j
+@WebFilter(urlPatterns = "/v1/*")
 public class AuthorizationFilter implements Filter {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${auth.jwt.service.url}")
+	private String authJwtServiceUrl;
 
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
@@ -35,7 +52,6 @@ public class AuthorizationFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
 			throws IOException, ServletException {
-
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
 		final String authorization = request.getHeader("Authorization");
@@ -49,10 +65,18 @@ public class AuthorizationFilter implements Filter {
 			} else {
 				setStatus(response);
 			}
-		}
-		else
-		{
-			setStatus(response);
+		} else {
+			final String uri = authJwtServiceUrl;
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Authorization", "Bearer " + authorization);
+			headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+			HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+			try {
+				restTemplate.exchange(uri, HttpMethod.POST, entity, String.class);
+				filterChain.doFilter(request, response);
+			} catch (Exception e) {
+				setStatus(response);
+			}
 		}
 
 	}
